4d0728e7a304e7ca36d3a35bd68b803e0a8b0e38
potter <potter.he@gmail.com>
2016-04-01 11:26:32 +0800

    rebase -i 1ae32ee6d49d589edb5008870f21fb5dd170ad30

--- cluster-up.sh
+++ cluster-up.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-export MASTER_IP="10.245.2.2"
-export MINION_IP="10.245.2.3"
+MASTER_IP="10.245.2.2"
+MINION_IP="10.245.2.3"
+MINION_NAME="minion-"${MINION_IP}
 
 if [[ -z ${DEVOPS_TEMP-} ]]; then
     export DEVOPS_TEMP=$(mktemp -d -t devops.XXXXXX)
     trap 'rm -rf "${DEVOPS_TEMP}"' EXIT
 fi
 
+# minion主机需要知晓master等数据，通过初始化脚本来传递。
+# 在vagrant场景，通过provision机制会复制脚本到VM里运行,以完成初始化.
 (
     echo "#! /bin/bash"
     echo "MASTER_IP='${MASTER_IP}'"
+    echo "MINION_NAME='${MINION_NAME}'"
     sed -e '/^#/d' provision-minion.sh
 ) > "${DEVOPS_TEMP}/minion-start.sh"
+
+export MASTER_IP
+export MINION_IP
 
 vagrant up
